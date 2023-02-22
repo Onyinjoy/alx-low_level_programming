bca64ed4e2e053886f15017c56b3b0f81d2a6bcf
Onyinjoy <joyfulhomes5@gmail.com>
2023-02-22 07:19:19 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main integral function
+ *
+ * Return: always return a 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
