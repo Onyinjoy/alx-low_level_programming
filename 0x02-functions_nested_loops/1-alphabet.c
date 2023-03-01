15ed42228a44ba414d169a31407d8cb49661f57b
Onyinjoy <joyfulhomes5@gmail.com>
2023-03-01 04:56:01 -0800

    alphabet lowwercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - main function of the program
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char o;
+
+	for (o = 'a' ; o <= 'z' ; o++)
+		_putchar(o);
+	_putchar('\n');
+}
