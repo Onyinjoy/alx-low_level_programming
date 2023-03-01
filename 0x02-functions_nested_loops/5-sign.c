762fd9fe8542e0eaeaba5dbcf107eaf29bd513a6
Onyinjoy <joyfulhomes5@gmail.com>
2023-03-01 07:11:36 -0800

    prints sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - prints signs
+ *
+ * @n: parameter of the program
+ *
+ * Return: 1 when it is greater than and o when it is zero
+ * or negative when it is less than
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
