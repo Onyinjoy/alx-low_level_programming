12cbda1be1cbe9a5e65862a8f411f07377c239f8
Onyinjoy <joyfulhomes5@gmail.com>
2023-03-01 04:19:08 -0800

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - main part of the peogram
+ * _putchar: print putchar with a new line
  * Return: always 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
