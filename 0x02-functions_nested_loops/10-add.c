407f1ec4be6e028ca4b12741dbfdd6908410628f
Onyinjoy <joyfulhomes5@gmail.com>
2023-03-02 05:47:56 -0800

    add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that adds numbers
+ * @a: parameters to be checked
+ * @b: parameter to be checked
+ * Return: always i
+ */
+int add(int a, int b)
+{
+	int i;
+
+	i = (a + b);
+	return (i);
+}
