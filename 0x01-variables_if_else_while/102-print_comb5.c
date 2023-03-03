ab3796de78687544bc311438577c907c41cd9198
Onyinjoy <joyfulhomes5@gmail.com>
2023-03-03 03:41:01 -0800

    largest number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,26 +9,3 @@ int main(void)
 	int j;
 
 	for (i = 0 ; i < 10 ; i++)
-	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			if (i < j && j>= i)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-			}
-			if (i >= j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i + j != 18)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
